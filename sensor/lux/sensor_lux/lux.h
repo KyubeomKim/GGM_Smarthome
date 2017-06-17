0f98246543b3ffbd6d2a948f440ffc47a63c3d94
KuybeomKim <kkb920408@naver.com>
2017-06-17 14:39:17 +0900

    폴더구조 변경

--- sensor/lux/sensor_lux/lux.h
+++
@@ -1,5 +0,0 @@
-void initLux(int _sensorPinNum);
-int getLux(int _bias=0);
-int getLuxScaleDown(int _bias=0);
-int getLuxReverse(int _bias=0);
-int getLuxScaleDownReverse(int _bias=0);
--- sensor/tilt/sensor_tilt/tilt.h
+++
@@ -1,3 +0,0 @@
-void initTilt(int _sensorPinNum);
-int tiltSwitchRead();
-int tiltSwitchReadInterval(int _interval);
