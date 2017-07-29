c1521fde3ad01d33fad12eaf1886558d3478168d
KyubeomKim <kkb920408@naver.com>
2017-07-29 14:45:48 +0900

    'commit'

---
+++ test/kyubeomkim/Serial_test/Serial_test.ino
@@ -0,0 +1,31 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial taekyun(4,3);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Serial Start");
+  taekyun.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(500);
+  // 수신
+  taekyun.listen();
+  if (taekyun.available()){
+    char c = (char)taekyun.read();
+    Serial.println(c);
+  }
+
+  // 송신
+  if (Serial.available()){
+    char c = Serial.read();
+    Serial.print("input value (print) : ");
+    Serial.println(c);
+    Serial.print("input value (write) : ");
+    Serial.write(c);
+    taekyun.write(49);
+  }
+}
